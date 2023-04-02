0ad9f215617501aaa99aa6dc7dc9d2d6fff61f99
Daniel Popchenko <104201822+DanielPopchenko@users.noreply.github.com>
2023-04-02 12:16:54 +0300

    add prettier file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles/App.css';
 import './styles/ItemsList.css';
 import './styles/Item.css';
 import ItemsList from './components/ItemsList';
 import data from './current-loans.json';
-import {formatAmountToNumber} from './components/utils/index'
-
-
-
+import { formatAmountToNumber } from './components/utils/index';
 
 function App() {
   const [loans, setLoans] = useState([]);
+
   useEffect(() => {
     setLoans(data.loans);
   }, []);
@@ -19,17 +17,18 @@ function App() {
     const available = formatAmountToNumber(i.available);
 
     return acc + available;
-  }, 0)
+  }, 0);
 
   return (
-
     <div className="App">
-      <h1 className='App-heading'>Our loans</h1>
+      <h1 className="App-heading">Our loans</h1>
       <hr></hr>
-      < ItemsList setLoans={setLoans} loans={loans} />
-      <p className='App-total'>Total amount available for investment: <span>${totalAmount.toFixed(3)}</span></p>
+      <ItemsList setLoans={setLoans} loans={loans} />
+      <p className="App-total">
+        Total amount available for investment: <span>${totalAmount.toFixed(3)}</span>
+      </p>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/ItemsList/index.jsx
+++ src/components/ItemsList/index.jsx
@@ -1,12 +1,11 @@
-
-import React, {useState} from 'react';
-import Item from '../Item/index'
+import React, { useState } from 'react';
+import Item from '../Item/index';
 import { formatAmountToNumber, formatAmountToString } from '../utils';
 
-import Modal from './../Modal/index'
-import './../Modal/modal.css'
+import Modal from './../Modal/index';
+import './../Modal/modal.css';
 
-const ItemsList = ({setLoans, loans}) => {
+const ItemsList = ({ setLoans, loans }) => {
   const [modalData, setModalData] = useState(null);
   const [modalActive, setModalActive] = useState(false);
 
@@ -14,34 +13,42 @@ const ItemsList = ({setLoans, loans}) => {
     const newLoans = [...loans];
     const data = newLoans.map((loan) => {
       if (loan.id === item.id) {
-        return {...loan, available : formatAmountToString(formatAmountToNumber(loan.available) - value), invested: true}
+        return {
+          ...loan,
+          available: formatAmountToString(formatAmountToNumber(loan.available) - value),
+          invested: true,
+        };
       } else {
         return loan;
       }
-    })
+    });
 
-    setLoans(data)
-    
-  }
-
+    setLoans(data);
+  };
 
   return (
-    <div className='loans-container'>
-      <ul className='loans-list'>
-        {loans.map((elem) => { 
+    <div className="loans-container">
+      <ul className="loans-list">
+        {loans.map((elem) => {
           return (
-            <Item key={elem.id} elem={elem} setModalData={setModalData} setModalActive={setModalActive} />
-          )
+            <Item
+              key={elem.id}
+              elem={elem}
+              setModalData={setModalData}
+              setModalActive={setModalActive}
+            />
+          );
         })}
       </ul>
-      {modalActive && <Modal data={modalData} setModalActive={setModalActive} handleInvest={handleInvest} />}
+      {modalActive && (
+        <Modal
+          data={modalData}
+          setModalActive={setModalActive}
+          handleInvest={handleInvest}
+        />
+      )}
     </div>
-
   );
 };
-
-
-
-
 
 export default ItemsList;
